2106b28237554aa66f92a6b6797eb872655d92f8
Nicholas Lau <nicholas.m.lau@gmail.com>
2015-12-15 15:22:11 -0800

    checks for platform to determine which mpg to call

--- modules/sound.py
+++ modules/sound.py
@@ -2,12 +2,14 @@
 
 import os
 import random
+import platform
 
 class Sound:
   def __init__(self, mp3_path):
     self.playing = False  # Since the command plays in background, stops multiple from running
     self.mp3_path = mp3_path
-  def soundAlarm(self):
+  
+  def start(self):
     if self.playing == False:
       print "Waking you up!"
       print "---" 
@@ -16,12 +18,17 @@ class Sound:
                                                      #  plays the MP3 in it's entierty. As long as the file is longer 
                                                      #  than a minute it will only be played once:
       # -g 100, sets sound to 100, & runs in background
-      command = "mpg123" + " " + self.mp3_path + "'"+songfile+"'"+ " -g 100 &" 
+      if platform.system() == 'Linux':
+        command = "mpg321"
+      else if platform.system() == 'Darwin':
+        command = "mpg123"
+      command += " " + self.mp3_path + "'"+songfile+"'"+ " -g 100 &"
+
       print command
       self.playing = True
       os.system(command)                             #  plays the song
 
-  def turnOffAlarm(self):
+  def stop(self):
     command = "pkill mpg123"
     print command
     self.playing = False
